7e21ad92b657ba6128cb6cfcff89aec7b90c78c8
ben28wettstein <wettsteinben@gmail.com>
2020-04-13 21:33:44 -0400

    Create skele.py

---
+++ skele.py
@@ -0,0 +1,99 @@
+""
+Name: Ben Wettstein, Victor Owale-Apanpa, Joe Magill
+File: skele.py
+
+Comments:
+
+This is a sample skeleton for a space blaster game. The overall game has been broken 
+into smaller tasks (functions) that need to be peformed. While most of the 
+functions are stubs and don't do anything at this point, the comments, parameter
+lists, and return values (or the lack of a return value, in the case of "pass"
+statements) show what they would do and how they would work. 
+
+
+Space blaster is a basic spaceship shooting game in which a ship is piloted 
+using a mouse-directed heading and thrusters initiated with the space bar.  The
+ship is capable of firing at enemies that appear randomly on the screen and 
+bounce back and forth.  The skeleton currently defines the important game features
+and primary functions.  This will be expanded to include power ups, boss enemies,
+and changes in the background with each level change. 
+
+The skeleton currently displays the ship, a sample bullet, and the alien to the 
+screen, along with the score.  All elements are static.  Additionally, 
+variables used for maintaining and altering game state have been defined at the 
+top of the file.  
+
+The project team includes CDTs Sulley and Wakowski.  No additional modules other
+than pygame zero are required to complete this project.
+
+"""
+
+"""
+Attribution:
+spaceship image: 
+
+"night raider" by dravenx licensed Creative Commons-BY 3.0: https://opengameart.org/content/night-raider
+"""
+
+
+
+
+scores = {}
+
+
+#Procedure: HRP
+# 
+#None->None
+def HRP():
+    """ This procedure performs the HRP event and then adds the score to the dictionary
+    """
+    pass
+
+#Procedure: SPT
+# 
+#None->None
+def SPT():
+    """ This procedure performs the SPT event and then adds the score to the dictionary
+    """
+    pass
+
+#Procedure: MDL
+# 
+#None->None    
+def MDL():
+    """ This procedure performs the MDL event and then adds the score to the dictionary
+    """
+    pass
+    
+#Procedure: SDC
+# 
+#None->None
+def SDC():
+    """ This procedure performs the SDC event and then adds the score to the dictionary
+    """
+    pass   
+
+#Procedure: LTK
+# 
+#None->None    
+def LTK():
+    """ This procedure performs the LTK event and then adds the score to the dictionary
+    """
+    pass    
+
+#Procedure: MR
+# 
+#None->None    
+def MR():
+    """ This procedure performs the MR event and then adds the score to the dictionary
+    """
+    pass    
+ 
+#Procedure: display_score
+# 
+#Dictonary->None 
+ def display_score(scores):
+    """ Displays each score individually, and then the total score
+    """
+    pass
+    
